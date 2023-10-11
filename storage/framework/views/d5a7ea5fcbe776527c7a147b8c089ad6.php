c5eef68f06cadd31692ab09bbbf2e374397ca23d
umashankardsmax <147578493+umashankardsmax@users.noreply.github.com>
2023-10-11 10:03:32 +0530

    Delete storage/framework directory

--- storage/framework/views/d5a7ea5fcbe776527c7a147b8c089ad6.php
+++
@@ -1,4 +0,0 @@
-<?php echo $__env->make('frontend.layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('main-container'); ?>
-<?php echo $__env->make('frontend.layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php /**PATH C:\Users\Branding\laundry\resources\views/frontend/layouts/main.blade.php ENDPATH**/ ?>
